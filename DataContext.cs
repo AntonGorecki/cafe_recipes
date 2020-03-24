2ad701ea13873726ecfb11ac6c2d32766024d748
Anton Horetskyi <goretskyas@gmail.com>
2020-03-24 21:41:51 +0100

    add data context

---
+++ DataContext.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DataContext : IDataContext
+    {
+        public string ConnectionString
+        {
+            get; private set;
+        }
+
+        public DataContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        public void ExecuteAction(Action<SqlConnection> action, SqlConnection connection = null)
+        {
+            if(connection != null)
+            {
+                action(connection);
+            }
+            else
+            {
+                using(var newConnection = new SqlConnection(ConnectionString))
+                {
+                    newConnection.Open();
+                    action(newConnection);
+                }
+            }
+        }
+
+        public T ExecuteCommand<T>(Func<SqlCommand, T> action)
+        {
+            using(var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                using(var command = connection.CreateCommand())
+                {
+                    return action(command);
+                }
+            }
+        }
+
+        public T ExecuteCommand<T>(SqlCommand cmd, Func<SqlCommand, T> action, SqlConnection connection = null, SqlTransaction transation = null)
+        {
+            if(connection != null)
+            {
+                cmd.Connection = connection;
+                if(transation != null)
+                {
+                    cmd.Transaction = transation;
+                }
+                return action(cmd);
+            }
+
+            using(var newConnecton = new SqlConnection(ConnectionString))
+            {
+                newConnecton.Open();
+                cmd.Connection = newConnecton;
+                if (transation != null)
+                {
+                    cmd.Transaction = transation;
+                }
+
+                return action(cmd);
+            }
+        }
+
+        public void ExecuteNonQueryCommand(SqlCommand cmd, SqlConnection connection = null, SqlTransaction transaction = null)
+        {
+            if(connection != null)
+            {
+                cmd.Connection = connection;
+                if (transaction != null)
+                {
+                    cmd.Transaction = transaction;
+                }
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                using(var newConnection = new SqlConnection(ConnectionString))
+                {
+                    newConnection.Open();
+                    cmd.Connection = newConnection;
+                    if(transaction != null)
+                    {
+                        cmd.Transaction = transaction;
+                    }
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ IDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+namespace DataLayer
+{
+    public interface IDataContext
+    {
+        string ConnectionString { get;}
+        void ExecuteAction(Action<SqlConnection> action, SqlConnection connection = null);
+        void ExecuteNonQueryCommand(SqlCommand cmd, SqlConnection connection = null, SqlTransaction transaction = null );
+        T ExecuteCommand<T>(Func<SqlCommand, T> action);
+        T ExecuteCommand<T>(SqlCommand cmd, Func<SqlCommand, T> action, SqlConnection connection = null, SqlTransaction transation = null);
+    }
+}
